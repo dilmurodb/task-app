5aa2c6f2b816a43bb574a257c2b8bc23e6228c3f
bukharov <dilmurod.bukharov@gmail.com>
2023-05-01 20:46:49 -0400

    created useState hook in TaskForm functional component, managed to update state in App parent component using callback functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import React, { useState } from 'react'
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList'
 
 function App() {
+
+  const [tasks, setTasks] = useState([{title: '', body: ''}])
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask])
+    console.log(tasks)
+  }
   return (
     <div className="App">
       <h1>task-app</h1>
-      <TaskForm />
-      <TaskList />
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TaskForm = () => {
+const TaskForm = ({addTask}) => {
+    const [taskTitle, setTaskTitle] = useState('')
+    const [taskBody, setTaskBody] = useState('')
+
+    const addNewTask = (e) => {
+        e.preventDefault()
+        const newTask = {title: taskTitle, body: taskBody}
+        addTask(newTask)
+        setTaskTitle('')
+        setTaskBody('')
+    }
+
   return (
     <form>
-        <input type='text' placeholder='Task title...'></input>
-        <input type='text' placeholder='Task description...'></input>
-        <button>Add task</button>
+        <input 
+            type='text' 
+            placeholder='Task title...' 
+            onChange={e => setTaskTitle(e.target.value)}
+            value={taskTitle}
+        ></input>
+        <input 
+            type='text' 
+            placeholder='Task description...' 
+            onChange={e => setTaskBody(e.target.value)}
+            value={taskBody}
+        ></input>
+        <button
+            onClick={addNewTask}>Add task</button>
     </form>
   )
 }
